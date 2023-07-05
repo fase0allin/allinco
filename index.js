5dc0b12ce7dd5e2547bf7b15dccc1e4a4c1b4da4
satriadega <satria.pandega@gmail.com>
2023-07-05 23:10:24 +0700

    ega rabu

--- index.js
+++ index.js
@@ -1,75 +1,84 @@
 let catalog = [
     {
+        id: 1,
         name: "White Shirt",
         price: 190000,
         image: "https://oxfoord.id/wp-content/uploads/2022/10/SHIRT-13A-NEW-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2022/10/light-blue1-247x296.jpg",
         quantity: 0,
-        rating : 4.7
+        rating: 4.7,
     },
     {
+        id: 2,
         name: "Flanel jedar",
         price: 150000,
         image: "https://oxfoord.id/wp-content/uploads/2023/04/Shirt-lapalmas-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/04/Shirt-lapalmas-4b-247x296.jpg",
         quantity: 0,
-        rating : 5
+        rating: 5,
     },
     {
+        id: 3,
         name: "Black Jeans",
         price: 259000,
         image: "https://oxfoord.id/wp-content/uploads/2021/09/jeans-black-1-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2021/09/jeans-black-2-247x296.jpg",
         quantity: 0,
-        rating : 4.9
+        rating: 4.9,
     },
     {
+        id: 4,
         name: "Navy Jeans",
         price: 185000,
         image: "https://oxfoord.id/wp-content/uploads/2022/08/jeans-navy-1-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2022/08/jeans-navy-3-247x296.jpg",
         quantity: 0,
-        rating : 5
+        rating: 5,
     },
     {
+        id: 5,
         name: "Casual T-Shirt",
         price: 195000,
         image: "https://oxfoord.id/wp-content/uploads/2023/05/POLO-1A-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/05/POLO-1B-247x296.jpg",
         quantity: 0,
-        rating : 4.6
+        rating: 4.6,
     },
     {
+        id: 6,
         name: "Black Elegant Shirt",
         price: 190000,
         image: "https://oxfoord.id/wp-content/uploads/2023/04/Shirt-7a-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/04/Shirt-7b-247x296.jpg",
         quantity: 0,
-        rating : 4.4
+        rating: 4.4,
     },
     {
+        id: 7,
         name: "Simple Pant",
         price: 190000,
         image: "https://oxfoord.id/wp-content/uploads/2023/03/Pants-3a-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/03/Pants-3c-247x296.jpg",
         quantity: 0,
-        rating : 4.8
+        rating: 4.8,
     },
     {
+        id: 8,
         name: "Why Jeans",
         price: 450000,
         image: "https://oxfoord.id/wp-content/uploads/2023/05/jeans-bluefade1-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/05/jeans-bluefade2-247x296.jpg",
         quantity: 0,
-        rating : 4.9
+        rating: 4.9,
     },
     {
+        id: 9,
         name: "Flanel Zeus",
         price: 380000,
         image: "https://oxfoord.id/wp-content/uploads/2023/03/pine-green-1-247x296.jpg",
         hover: "https://oxfoord.id/wp-content/uploads/2023/03/pine-green-2-247x296.jpg",
         quantity: 0,
-        rating : 4.8
+        rating: 4.8,
     },
 ];
 
@@ -132,7 +141,7 @@ for (let i = 0; i < catalog.length; i++) {
     ratingImage.src = "./assets/stars.png";
 
     const rating = document.createElement("p");
-    rating.textContent = catalog[i].rating
+    rating.textContent = catalog[i].rating;
 
     nameSection.appendChild(itemName);
     nameSection.appendChild(ratingImage);
@@ -149,7 +158,7 @@ for (let i = 0; i < catalog.length; i++) {
     const cartSection = document.createElement("div");
     cartSection.classList.add("cart");
 
-    const addToCart = document.createElement("p");
+    const addToCart = document.createElement("button");
     addToCart.textContent = "Add to Cart";
 
     cartSection.appendChild(addToCart);
@@ -157,13 +166,9 @@ for (let i = 0; i < catalog.length; i++) {
     card.appendChild(cartSection);
 
     parentElement.appendChild(card);
-
 }
 
-
 // const parentElement = document.getElementById("container");
-
-
 
 // for (let i = 0; i < catalog.length; i++) {
 //     const card = document.createElement("div");
@@ -180,3 +185,14 @@ for (let i = 0; i < catalog.length; i++) {
 //     // Append the card to the parent element
 //     parentElement.appendChild(card);
 // }
+
+const windowPop = document.getElementById("cartdiv");
+windowPop.style.display = "none";
+
+function toggleCart() {
+    windowPop.style.display = "";
+}
+
+function clickClose() {
+    windowPop.style.display = "none";
+}
