113f610ccef8d6ca895890c56a01a6778e5bf994
arbi <arbiihsan@gmail.com>
2023-07-06 13:11:25 +0700

    arbiihsan

--- index.js
+++ index.js
@@ -71,3 +71,31 @@ for (let i = 0; i < catalog.length; i++) {
     card.classList.add("test");
     parentElement.appendChild(card);
 }
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  
+    // Retrieve form input values
+    const nameInput = document.getElementById('name');
+    const priceInput = document.getElementById('price');
+    const linkInput = document.getElementById('link');
+  
+    // Create a new object and assign input values to properties
+
+    const newObj = {
+        name: nameInput.value,
+        price: parseInt(priceInput.value),
+        image: linkInput.value,
+        hover: "",
+        quantity: 0,
+    }
+  
+    // Push the object to the array
+    catalog.push(newObj);
+  
+    // Reset the form
+    document.getElementById('myForm').reset();
+  
+    // Perform any additional operations with the array of objects
+    console.log(catalog);
+  }
